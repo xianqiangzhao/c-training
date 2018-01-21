4ff4137943477face9b2dab7a75ef9a6999fa310
zhaoxianqiang <24930662@qq.com>
2018-01-21 22:16:33 +0800

    update

--- fileio/bigfile.c
+++ fileio/bigfile.c
@@ -10,16 +10,16 @@ int main(int argc, char const *argv[])
         fprintf(stderr, "Usage:%s  <write content>  <filename>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
-   	char  *w = (char *)argv[1];
-  	char  *filename = (char *)argv[2];
-  	int  fd;
-  	int n;
-  	fd = open(filename, O_CREAT|O_RDWR);
-  	if (fd < 0)
-  		perror("open file error");
+  char  *w = (char *)argv[1];
+  char  *filename = (char *)argv[2];
+  int  fd;
+  int n;
+  fd = open(filename, O_CREAT|O_RDWR);
+  if (fd < 0)
+  	perror("open file error");
   	//printf(" buf size is = %d\n",size);
-  	struct stat statbuf;
-  	fstat(fd, &statbuf);
+  struct stat statbuf;
+  fstat(fd, &statbuf);
  	int filesize;
  	filesize = statbuf.st_size/1024/1024;
  	printf("%d  k\n", filesize);
@@ -31,6 +31,6 @@ int main(int argc, char const *argv[])
  		filesize = statbuf.st_size/1024/1024;
 
  	}
- 	 exit(0);
+ 	exit(0);
  	
 }
