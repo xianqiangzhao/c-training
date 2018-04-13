05289aecfa0e31cc935611cdab677cf1af38feb0
zhaoxianqiang <24930662@qq.com>
2018-04-13 18:02:43 +0800

    update

--- mutex/process_mutex.c
+++ mutex/process_mutex.c
@@ -23,14 +23,14 @@ struct _shared
 	pthread_mutex_t *sum_mutex;
 	int * shared_sum;
 }    ;
-struct _shared  p_map , p_map1,* shared_map;
+struct _shared  p_map , *p_map1,* shared_map;
    
 int process_fn() {
-	int i;
-	for (i = 0; i < 100; i++){
-		pthread_mutex_lock(p_map.sum_mutex); //如果注释掉，shared_sum 的结果将是不正确的。因为父子进程共享的shared_sum
-		*(p_map.shared_sum) += 1;
-		pthread_mutex_unlock(p_map.sum_mutex);
+	int i;			
+	for (i = 0; i < 2; i++){
+		pthread_mutex_lock(p_map1->sum_mutex); //如果注释掉，shared_sum 的结果将是不正确的。因为父子进程共享的shared_sum
+		*(p_map1->shared_sum) += 1;
+		pthread_mutex_unlock(p_map1->sum_mutex);
 	}
 	return 0;
 }
@@ -43,11 +43,12 @@ int main(int argc, char const *argv[])
 	void * pt;
 	pthread_mutex_t   mutex;
 	int sum = 0;
- 	pt = mmap(NULL, sizeof(_shared), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
- 	bzero(&p_map, sizeof(_shared));
+ 	pt = mmap(NULL, sizeof(p_map), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+ 	bzero(&p_map, sizeof(p_map));
 	p_map.sum_mutex = (pthread_mutex_t * )pt;
-	p_map.shared_sum = (int * )pt + 1;
-	memcpy(pt, &p_map, sizeof(p_map));
+	p_map.shared_sum = (int  * )(pt + 4);
+	p_map1 = &p_map;
+	memcpy(pt, &p_map1, sizeof(p_map));
 	//shared_map = &p_map;
 	//shared_sum = (int * ) (p_map + 3); //这句打开是结果是20000000 ，父子进程共享这个指针。
 	//shared_sum = &temp; //这句打开是结果是10000000 也就是父子进程的 shared_sum 没有共享。
@@ -62,6 +63,6 @@ int main(int argc, char const *argv[])
 		process_fn();
 	}
 	wait(NULL);
-	printf(" in the end the shared sum: %d  ", *p_map.shared_sum);
+	printf(" in the end the shared sum: %d  ", *p_map1->shared_sum);
 	return 0;
 }
