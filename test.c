eb23c59cf19a2cd934b5643b6a46428d380a40a5
root <24930662@qq.com>
2018-05-03 14:58:12 +0000

    add

--- test.c
+++ test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+ 
 int main(){
+  
+   int a,*p;
+   p = &a;
+   a = 10;
+   printf("%d\n",*p );
 
-   printf("%f", 0.1+0.2);
+   int a1 = 200 ,c = 12, d = 13;
+   void **p1[2];
+    p1[1] = malloc(sizeof(void *) * 128);
+    p1[1][0] =  &a1;
+    printf("%d\n",  *(int *) p1[1][0] );
+    //p1 = p1 +1;
+
    return 0;
 
 }
