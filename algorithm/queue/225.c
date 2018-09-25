84b7db910d6b141f90eeacad624af636f4d09eb3
zhaoxianqiang <24930662@qq.com>
2018-09-25 13:44:04 +0800

    update

--- algorithm/queue/225.c
+++ algorithm/queue/225.c
@@ -4,6 +4,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+enum {
+  STACK_OK,
+  STACK_ERR,
+};
 typedef int QElemType;
 typedef struct QNode
 {
@@ -15,48 +19,117 @@ typedef struct {
     QueuePtr front;
     QueuePtr rear;
     int maxSize;
+    int num;
 } MyStack;
 
 /** Initialize your data structure here. */
 MyStack* myStackCreate(int maxSize) {
     MyStack *stack = malloc(sizeof(MyStack));
+    bzero(stack, sizeof(MyStack));
     stack->maxSize = maxSize;
     stack->front = stack->rear;
 }
 
 /** Push element x onto stack. */
 void myStackPush(MyStack* obj, int x) {
-        QueuePtr *node = malloc(sizeof(QNode));
-        node->data = x;
-        node->next = null;
-        obj->rear->next = node;
-		obj->rear = node;
+    if (obj->num >= obj->maxSize) {
+        obj = realloc(obj, obj->maxSize *2);
+        obj->maxSize *= 2;
 
+    }
+    QueuePtr node = malloc(sizeof(QNode));
+    node->data = x;
+    node->next = NULL;
+    if (!obj->rear) {
+        obj->rear = node;
+    }
+    else {
+       obj->rear->next = node;
+    }
+    obj->rear = node;
+    if (!obj->front) {
+     obj->front = obj->rear;
+    }
+    obj->num++;      
 }
 
 /** Removes the element on top of the stack and returns that element. */
 int myStackPop(MyStack* obj) {
-     QueuePtr *node = obj->front->next;
-     obj->front->next = node->next;
-     if (node == obj->rear)
-     	obj->front = obj->rear;
+    if (obj->front) {
+        QueuePtr node = obj->front;
+        obj->front = node->next;
+         if (node = obj->rear) {
+             obj->rear = obj->front;
+        }
+        obj->num--;
+        return  node->data;
+    } else {
+        return STACK_ERR;
+    }
+
 }
 
 /** Get the top element. */
 int myStackTop(MyStack* obj) {
-    return  obj->front->next->data;
+    if (obj->front){        
+        return obj->front->data;
+
+    } else{
+        return  STACK_ERR;
+    }
 }
 
 /** Returns whether the stack is empty. */
 int myStackEmpty(MyStack* obj) {
-    retrun (obj->front == obj->next) ? 1:0;
+    return (obj->front - obj->rear ) ? 1:0;
 }
 
 void myStackFree(MyStack* obj) {
-    where(obj->front->next) {
-    	QueuePtr *node  = obj->front->next;
-    	obj->front->next = node->next;
+
+    while(obj->front){
+    	QueuePtr node  = obj->front;
+    	obj->front = node->next;
     	free(node);
     }
     free(obj);
+}
+
+
+void printStack(MyStack *obj){
+    QueuePtr tmp = obj->front;
+    while(tmp){
+        printf("%d\n",tmp->data);
+        tmp = tmp->next;
+    }
+    printf("stack size = %d num = %d\n", obj->maxSize, obj->num);
+
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    MyStack *stack = myStackCreate(2);
+    myStackPush(stack, 100);
+    // myStackPush(stack, 200);
+    printf("stack is empty = %d\n", myStackEmpty(stack));
+    printf("stack top data =  %d\n", myStackTop(stack));
+    myStackPop(stack);
+    printf("stack is empty = %d\n", myStackEmpty(stack));
+    printf("stack top data =  %d\n", myStackTop(stack));
+    myStackPush(stack, 100);
+    myStackPush(stack, 200);
+    myStackPush(stack, 300);
+    myStackPush(stack, 400);
+    myStackPush(stack, 500);
+    printStack(stack);
+    myStackPush(stack, 600);
+    printStack(stack);
+
+    myStackPop(stack);
+    myStackPop(stack);
+    myStackPop(stack);
+    myStackPop(stack);    
+    printStack(stack);
+
+    return 0;
 }
